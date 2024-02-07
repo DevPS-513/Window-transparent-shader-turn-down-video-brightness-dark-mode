43cb1c6e0f07a7d807f5cdce8defde784e744fff
DevPS-513 <max.bethune.waddell@gmail.com>
2024-02-07 12:08:12 -0500

    added picture example and readme

---
+++ README.md
@@ -0,0 +1,5 @@
+## Window Shader
+
+Run this python app to create a window that you can adjust the brightness with
+
+![example](./example.png)
--- tkinter_window_shades.py
+++ tkinter_window_shades.py
@@ -1,22 +1,29 @@
-from tkinter import Tk, Scale
+from tkinter import Tk, Label, BOTTOM
 
 # Create the main window
 win = Tk()
 win.configure(background='black')
+opacity_start = 0.65
+
 win.wm_attributes("-alpha", .65)
 win.geometry("700x350")
 
+# Create a label to display the opacity
+opacity_label = Label(win, text=f"<<left arrow--Opacity: {opacity_start * 100:.0f}--right arrow>>%", bg="grey")
+opacity_label.pack(side=BOTTOM)
+
 # Function to change the opacity of the main window
 def change_opacity(value):
-    win.wm_attributes("-alpha", float(value))
+    current_opacity = float(win.wm_attributes("-alpha"))
+    new_opacity = current_opacity + value
+    new_opacity = min(max(new_opacity, 0.0), 1.0)  # Ensure the opacity stays between 0.0 and 1.0
+    win.wm_attributes("-alpha", new_opacity)
+    opacity_label.config(text=f"<<left arrow--Opacity: {new_opacity * 100:.0f}%--right arrow>>")  # Update the label text
 
-# Create a new window for the scale widget
-scale_win = Tk()
-scale_win.geometry("+0+500")  # Position the window at the bottom left of the screen
+# Bind the left arrow key to a function that decreases the opacity
+win.bind('<Left>', lambda event: change_opacity(-0.01))
 
-# Create a new Scale widget in the scale window
-opacity_scale = Scale(scale_win, from_=0.0, to=1.0, resolution=0.01, orient="vertical", command=change_opacity)
-opacity_scale.set(.65)  # Set the initial value
-opacity_scale.pack()
+# Bind the right arrow key to a function that increases the opacity
+win.bind('<Right>', lambda event: change_opacity(0.01))
 
 win.mainloop()
